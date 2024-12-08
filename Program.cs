50dd84d6368d61f6f504e9dea401ae3a4861fab4
yuchichen777 <yuchichen777@gmail.com>
2024-12-08 19:35:55 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DelegateSample
+{
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               MyClass myClass = new MyClass();
+               MyDelegate myDelegate = new MyDelegate(myClass.Method1);
+
+               myDelegate += myClass.Method2;
+
+               myDelegate("Hello, World!");
+          }
+
+          public delegate void MyDelegate(string message);
+
+          public class MyClass
+          {
+               public void Method1(string message)
+               {
+                    Console.WriteLine($"Method1 says: {message}");
+               }
+
+               public void Method2(string message)
+               {
+                    Console.WriteLine($"Method2 says: {message}");
+               }
+          }
+
+     }
+}
